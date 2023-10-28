10a79fedddc2cd4795e8d8ec900a8d6c841f0d71
TuhinLK <tuhinh02@gmail.com>
2023-10-28 23:44:28 +0600

    test case created

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,23 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :users
+
+  test "should be valid" do
+    user = users(:valid_user)
+    assert user.valid?
+  end
+
+  test "should require a first name" do
+    user = User.new(last_name: 'Doe', email_id: 'user@example.com')
+    assert_not user.valid?
+  end
+
+  test "should require a unique email" do
+    existing_user = users(:valid_user)
+    user = User.new(first_name: 'Someone', last_name: 'User', email_id: existing_user.email_id)
+    assert_not user.valid?
+  end
+
+
 end
