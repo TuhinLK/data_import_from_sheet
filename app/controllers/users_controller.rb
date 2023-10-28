eaa834851649fb0b55b03cbf2c884ab60f895ecc
TuhinLK <tuhinh02@gmail.com>
2023-10-28 23:14:42 +0600

    showing total data count total accept count failure count and reason of failure

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   end
   def import_users
     if params[:excel_file].present?
-      failed_data = ""
+      failed_data = ''
       file = params[:excel_file]
       xlsx = case File.extname(file.original_filename)
              when ".csv" then Roo::Spreadsheet.open(file, extension: :csv)
@@ -30,15 +30,16 @@ class UsersController < ApplicationController
           accept_count += 1
         else
           p @user.errors.full_messages
+          failed_data +=  "##{i} number row data: #{xlsx.row(i + 1).join(' ')}, Errors: #{@user.errors.full_messages.join(' ')}*"
           failed_count += 1
-          @user.errors.each do |error|
-            failed_data += "#{error.full_message}"
-          end
         end
       end
       p "*************************************"
       p failed_data
-      redirect_to root_path
+      overall_message = "Total Rows in sheet: #{count - 1}, Total Accepted Rows: #{accept_count}, Total Failed Rows: #{failed_count}*"
+      overall_message += "Reason of failure:*"
+      overall_message += failed_data
+      redirect_to root_path, notice: overall_message
     end
   end
 
